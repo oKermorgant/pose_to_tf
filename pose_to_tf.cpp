07f27ad16affcb22c0f1403cdf7ce59d6763e8a7
Olivier Kermorgant <olivier_kermorgant@yahoo.fr>
2022-05-31 16:24:00 +0200

    no temporary header

--- pose_to_tf.cpp
+++ pose_to_tf.cpp
@@ -19,120 +19,121 @@ using namespace std;
 
 namespace pose_to_tf
 {
-using nav_msgs::msg::Odometry;
+  using nav_msgs::msg::Odometry;
 
-class Pose2TF : public rclcpp::Node        
-{
-  tf2_ros::TransformBroadcaster br;
-  geometry_msgs::msg::TransformStamped tf;
-
-  rclcpp::SubscriptionBase::SharedPtr pose_sub;
-  rclcpp::TimerBase::SharedPtr topic_timer;
-
-  string topic, parent_frame, child_frame;
-
-
-
-public:
-  explicit Pose2TF(rclcpp::NodeOptions options):
-    Node("pose_to_tf", options),
-    br(this),
-    topic         {declare_parameter<string>("topic", "pose_gt")},
-    parent_frame  {declare_parameter<string>("parent_frame", "world")},
-    child_frame   {declare_parameter<string>("child_frame", "base_link")}
+  class Pose2TF : public rclcpp::Node
   {
-    // topic to absolute in order to find it in list
-    if(topic[0] != '/')
-      topic = string(get_namespace()) + "/" + topic;
+    tf2_ros::TransformBroadcaster br;
+    geometry_msgs::msg::TransformStamped tf;
 
-    topic_timer = create_wall_timer(500ms, [&](){detectTopicType();});
-  }
+    rclcpp::SubscriptionBase::SharedPtr pose_sub;
+    rclcpp::TimerBase::SharedPtr topic_timer;
 
-private:
+    string topic, parent_frame, child_frame;
 
-  inline Header toHeader(const string &parent_frame)
-  {
-    Header header;
-    header.stamp = get_clock()->now();
-    header.frame_id = parent_frame;
-    return header;
-  }
 
-  void republish(const Header &header,
-                 const string &child_frame,
-                 const Transform &pose)
-  {
-    tf.header = header;
-    tf.child_frame_id = child_frame;
-    tf.transform = pose;
-    br.sendTransform(tf);
-  }
 
-  void republish(const Header &header,
-                 const string &child_frame,
-                 const Pose &pose)
-  {
-    tf.header = header;
-    tf.child_frame_id = child_frame;
-    tf.transform.translation.x = pose.position.x;
-    tf.transform.translation.y = pose.position.y;
-    tf.transform.translation.z = pose.position.z;
-    tf.transform.rotation = pose.orientation;
-    br.sendTransform(tf);
-  }
-
-  void detectTopicType()
-  {
-    const auto topics{get_topic_names_and_types()};
-    const auto info{topics.find(topic)};
-
-    if(info == topics.end())
-      return;
-
-    topic_timer.reset();
-
-    if(info->second.size() > 1)
+  public:
+    explicit Pose2TF(rclcpp::NodeOptions options):
+      Node("pose_to_tf", options),
+      br(this),
+      topic         {declare_parameter<string>("topic", "pose_gt")},
+      parent_frame  {declare_parameter<string>("parent_frame", "world")},
+      child_frame   {declare_parameter<string>("child_frame", "base_link")}
     {
-      RCLCPP_WARN(get_logger(), topic.c_str(), "seems to have several types of message");
+      // topic to absolute in order to find it in list
+      if(topic[0] != '/')
+        topic = string(get_namespace()) + "/" + topic;
+
+      topic_timer = create_wall_timer(500ms, [&](){detectTopicType();});
     }
 
-    const auto msg{info->second[0]};
+  private:
 
-    if(msg == "geometry_msgs/msg/Pose")
+    inline void republish(const string &child_frame,
+                          const Transform &pose)
     {
-      pose_sub = create_subscription<Pose>(topic, 1, [&](Pose::SharedPtr msg)
-      {republish(toHeader(parent_frame), child_frame, *msg);});
+      tf.child_frame_id = child_frame;
+      tf.transform = pose;
+      br.sendTransform(tf);
     }
-    else if(msg == "geometry_msgs/msg/pose_stamped")
+
+    inline void republish(const string &child_frame,
+                          const Pose &pose)
     {
-      pose_sub = create_subscription<PoseStamped>(topic, 1, [&](PoseStamped::SharedPtr msg)
-      {republish(toHeader(parent_frame), msg->header.frame_id, msg->pose);});
+      tf.child_frame_id = child_frame;
+      tf.transform.translation.x = pose.position.x;
+      tf.transform.translation.y = pose.position.y;
+      tf.transform.translation.z = pose.position.z;
+      tf.transform.rotation = pose.orientation;
+      br.sendTransform(tf);
     }
-    else if(msg == "geometry_msgs/msg/Transform")
+
+    void detectTopicType()
     {
-      pose_sub = create_subscription<Transform>(topic, 1, [&](Transform::SharedPtr msg)
-      {republish(toHeader(parent_frame), child_frame, *msg);});
+      const auto topics{get_topic_names_and_types()};
+      const auto info{topics.find(topic)};
+
+      if(info == topics.end())
+        return;
+
+      topic_timer.reset();
+
+      if(info->second.size() > 1)
+        {
+          RCLCPP_WARN(get_logger(), topic.c_str(), "seems to have several types of message");
+        }
+
+      const auto msg{info->second[0]};
+
+      if(msg == "geometry_msgs/msg/Pose")
+        {
+          pose_sub = create_subscription<Pose>(topic, 1, [&](Pose::SharedPtr msg)
+          {
+              tf.header.frame_id = parent_frame;
+              tf.header.stamp = get_clock()->now();
+              republish(child_frame, *msg);});
+        }
+      else if(msg == "geometry_msgs/msg/pose_stamped")
+        {
+          pose_sub = create_subscription<PoseStamped>(topic, 1, [&](PoseStamped::SharedPtr msg)
+          {
+              tf.header.frame_id = parent_frame;
+              tf.header.stamp = msg->header.stamp;
+              republish(msg->header.frame_id, msg->pose);});
+        }
+      else if(msg == "geometry_msgs/msg/Transform")
+        {
+          pose_sub = create_subscription<Transform>(topic, 1, [&](Transform::SharedPtr msg)
+          {
+              tf.header.frame_id = parent_frame;
+              tf.header.stamp = get_clock()->now();
+              republish(child_frame, *msg);});
+        }
+      else if(msg == "geometry_msgs/msg/TransformStamped")
+        {
+          pose_sub = create_subscription<TransformStamped>(topic, 1, [&](TransformStamped::SharedPtr msg)
+          {
+              tf.header = msg->header;
+              republish(msg->child_frame_id, msg->transform);});
+        }
+      else if(msg == "nav_msgs/msg/Odometry")
+        {
+          pose_sub = create_subscription<Odometry>(topic, 1, [&](Odometry::SharedPtr msg)
+          {
+              tf.header = msg->header;
+              republish(msg->child_frame_id, msg->pose.pose);});
+        }
+      else
+        {
+          RCLCPP_ERROR(get_logger(), topic.c_str(), " has unsupported message type ", msg.c_str());
+        }
     }
-    else if(msg == "geometry_msgs/msg/TransformStamped")
-    {
-      pose_sub = create_subscription<TransformStamped>(topic, 1, [&](TransformStamped::SharedPtr msg)
-      {republish(msg->header, msg->child_frame_id, msg->transform);});
-    }
-    else if(msg == "nav_msgs/msg/Odometry")
-    {
-      pose_sub = create_subscription<Odometry>(topic, 1, [&](Odometry::SharedPtr msg)
-      {republish(msg->header, msg->child_frame_id, msg->pose.pose);});
-    }
-    else
-    {
-      RCLCPP_ERROR(get_logger(), topic.c_str(), " has unsupported message type ", msg.c_str());
-    }
-  }
 
 
 
 
-};
+  };
 }
 
 // boilerplate main
